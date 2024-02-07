94860ea22df7bcc66185542b0d6f3abed585fb83
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-02-07 07:06:25 +0700

    create footer comopnent

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+export function Footer() {
+  return (
+    <footer className='text-center'>
+      <p className='text-balance text-xs font-medium text-gray-500'>
+        Created with ❤ by{' '}
+        <a href='https://rizkicitra.dev' target='_blank' rel='noopener noreferrer'>
+          Rizki M Citra
+        </a>
+      </p>
+    </footer>
+  )
+}
