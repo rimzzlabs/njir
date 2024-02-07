c1fe65904e8b7047051c3e5ab462f5a08e032fbe
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-02-07 07:10:10 +0700

    style: set container width to 11/12

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,3 +1,3 @@
 export function Layout(props: React.PropsWithChildren) {
-  return <div className='max-w-4xl mx-auto w-[98%] pb-4 pt-24'>{props.children}</div>
+  return <div className='max-w-4xl mx-auto w-11/12 pb-4 pt-24'>{props.children}</div>
 }
