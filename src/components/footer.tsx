aa36b4e9268aead9aa43953572e46548bed4a02f
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-02-07 07:46:03 +0700

    feat: added explanation about the web

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,17 @@
 export function Footer() {
   return (
-    <footer className='text-center'>
-      <p className='text-balance text-xs font-medium text-gray-500'>
+    <footer className='text-center text-xs px-10 py-4'>
+      <p className='text-balance mb-2 font-medium text-rose-800'>
+        Perhatian! web ini hanyalah contoh penerapan SIG untuk memenuhi tugas kuliah.
+      </p>
+      <p className='text-balance text-gray-500'>
         Created with ❤ by{' '}
-        <a href='https://rizkicitra.dev' target='_blank' rel='noopener noreferrer'>
+        <a
+          href='https://rizkicitra.dev'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='underline decoration-dashed'
+        >
           Rizki M Citra
         </a>
       </p>
