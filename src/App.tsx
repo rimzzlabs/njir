9fec5b98f54eb9ea013947f69569c26071895715
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-02-07 07:10:30 +0700

    feat: display faq and footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { Header } from '@/components/header'
 import { Layout } from '@/layout'
 import { queryClient } from '@/lib/utils'
 
+import { Faq } from './components/faq'
+import { Footer } from './components/footer'
+
 import { QueryClientProvider } from '@tanstack/react-query'
 
 export function App() {
@@ -13,6 +16,8 @@ export function App() {
       <Layout>
         <Header />
         <Gis />
+        <Faq />
+        <Footer />
       </Layout>
       <GisDialog />
     </QueryClientProvider>
